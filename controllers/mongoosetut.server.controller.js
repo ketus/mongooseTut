5f6264848b51835e303c32bb75776117a58ebaa1
Ketus-home <maciej.oleksa@gmail.com>
2015-12-20 22:21:18 +0100

    deleted dependencies

--- controllers/mongoosetut.server.controller.js
+++ controllers/mongoosetut.server.controller.js
@@ -1,3 +1,20 @@
 /**
  * Created by ketus on 12/20/15.
  */
+var Ctrl = require('../models/mongoosetut.server.model.js');
+
+exports.create = function (req, res) {
+	var entry = new Ctrl({
+		memberName: req.body.memberName,
+		project: req.body.project,
+		workYesterday: req.body.workYesterday,
+		workToday: req.body.workToday,
+		impediment: req.body.impediment
+
+	});
+
+	entry.save();
+
+	res.redirect(301, '/');
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongooseCtrl = require('../controllers/mongoosetut.server.controller.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/newnote', function (req, res) {
+	return mongooseCtrl.getNote(req, res);
+});
+
 
 module.exports = router;
